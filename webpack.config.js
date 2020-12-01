02cffbfcaea2db91f0c1a7397dc9527e23fdb1e6
Gabriel Linhares <glinharesb@gmail.com>
2020-12-01 10:09:58 -0300

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, "./dist"),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+        writeToDisk: true,
+    },
+    entry: {
+        main: path.resolve(__dirname, "./src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].bundle.min.js",
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
+    },
+};
